81551af6d72ecc8e1578e04b68bcd1b385a82453
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-02-21 01:13:50 +0530

    adding props

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-import './App.css';
+import TodoItem from "./components/TodoItem";
+
+import "./App.css";
 
 function App() {
+  const todoItem = [
+    {
+      name: "Buy Milk",
+      dueDate: "20/02/24",
+    },
+    {
+      name: "Commite Suicide",
+      dueDate: "20/02/24",
+    },
+  ];
   return (
-    <center className="todo-content">
+    <center className="todo-container">
       <AppName />
-        <AddTodo />
-        <div class="row">
-        <div class= "col-6">
-          Buy Milk
-        </div>
-          <div class="col-4">
-            4/10/24
-          </div>
-          <div class="col-2">
-            <button type="button" className="btn btn-danger">Delete</button>
-          </div>
-        </div>
-        <div class="row">
-        <div class= "col-6">
-          Commit Suicide
-        </div>
-          <div class="col-4">
-            20/02/24
-          </div>
-          <div class="col-2">
-            <button type="button" className="btn btn-danger">Delete</button>
-          </div>
-        </div>
+      <AddTodo />
+      <div className="items-container">
+        <TodoItem todoName="Buy Milk" todoDate="20/02/24"></TodoItem>
+        <TodoItem todoName="Commit Suicide" todoDate="20/02/24"></TodoItem>
+      </div>
     </center>
   );
 }
