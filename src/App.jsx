b1e821ef8b1477d50df8646c658dcacf7ea47456
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-03-03 23:23:59 +0530

    complete project done

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,18 @@ function App() {
     ];
     setTodoItems(newTodoItems);
   };
+
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    setTodoItems(newTodoItems);
+  };
+
   return (
     <center className="todo-container">
       <AppName />
       <AddTodo onNewItem={handleNewItem} />
-      {todoItems.length >= 0 && <WelcomeMessage />}
-      <TodoItems todoItems={todoItems} />
+      {todoItems.length === 0 && <WelcomeMessage />}
+      <TodoItems todoItems={todoItems} onDeleteClick={handleDeleteItem} />
     </center>
   );
 }
