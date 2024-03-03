1f45c91503c238955693864967011823b0773709
Anant Singh Raghuvanshi <anantsinghraghuvanshi1@gmail.com>
2024-03-03 23:23:49 +0530

    delete functionality done

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,10 +1,14 @@
 import TodoItem from "./TodoItem";
 
-const TodoItems = ({ todoItems }) => {
+const TodoItems = ({ todoItems, onDeleteClick }) => {
   return (
     <div className="items-container">
       {todoItems.map((item) => (
-        <TodoItem todoName={item.name} todoDate={item.dueDate}></TodoItem>
+        <TodoItem
+          todoName={item.name}
+          todoDate={item.dueDate}
+          onDeleteClick={onDeleteClick}
+        ></TodoItem>
       ))}
     </div>
   );
